9929a4705d14b785f16235bcb70c21659b0625a1
Nikhil <nikhilgodhwani@gmail.com>
2023-10-23 02:08:47 +0530

    model training added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,3 +19,17 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: Path
+    params_batch_size: Path
+    params_is_augmentation: bool
+    params_image_size: list
+
+
